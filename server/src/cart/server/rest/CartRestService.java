53ac33429920e2d4c8130461a71d4462dfaea73f
Praveen Kumar Pendyala <praveendath92@gmail.com>
2015-11-28 01:34:50 +0100

    Server REST skeleton added

---
+++ server/src/cart/server/rest/CartRestService.java
@@ -0,0 +1,47 @@
+package cart.server.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
+
+@Path("/shop")
+public class CartRestService {
+
+	@GET
+	@Path("/user/login")
+	public String login() {
+		return "User login!";
+	}
+	
+	@GET
+	@Path("/user/logout")
+	public String logout(@QueryParam("userid") int userid) {
+		return "User lgout for userid : " + userid;
+	}
+	
+	@GET
+	@Path("/product/list")
+	public String listProducts() {
+		return "Product list";
+	}
+
+	@GET
+	@Path("/product/{productid}/addToCart")
+	public String addToCart(@PathParam("productid") int productid, @QueryParam("userid") int userid) {
+		return "Product : " + productid + " userid : " + userid;
+	}
+	
+	@GET
+	@Path("/cart/list")
+	public String addToCart(@QueryParam("userid") int userid) {
+		return "Cartlisting for userid : " + userid;
+	}
+	
+	@GET
+	@Path("/cart/checkout")
+	public String checkout(@QueryParam("userid") int userid) {
+		return "Cart checkout for userid : " + userid;
+	}
+
+}
